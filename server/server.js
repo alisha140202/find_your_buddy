c5051c9b28598f3fd4ff323e195f927e7b3d8695
unknown <alishakesarwani14@gmail.com>
2024-03-22 00:51:12 +0530

    file added

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3780;
 
 const __dirname = path.resolve();
 
@@ -29,7 +29,42 @@ app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
-server.listen(PORT, () => {
-	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
+// Global error handling for uncaught exceptions
+process.on('uncaughtException', (err) => {
+	console.error('Unhandled Exception:', err);
+	// Perform any necessary cleanup or logging here
+	process.exit(1);
 });
+
+// Global error handling for unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+	console.error('Unhandled Promise Rejection:', err);
+	// Perform any necessary cleanup or logging here
+});
+
+// Error handling middleware for Express
+app.use((err, req, res, next) => {
+	console.error('Error:', err);
+	res.status(500).send('Internal Server Error');
+});
+
+// Attempt to start the server
+try {
+	server.listen(PORT, () => {
+		connectToMongoDB();
+		console.log(`Server Running on port ${PORT}`);
+	});
+	server.on('error', (error) => {
+
+	if (error.code === 'EADDRINUSE') {
+		console.error(`Port ${PORT} is already in use.`);
+		process.exit(1);
+	} else {
+		console.error('Error starting server:', error);
+		process.exit(1);
+	}
+})
+} catch (error) {
+	console.error('Error starting server:', error);
+		process.exit(1);
+}
